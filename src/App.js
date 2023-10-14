5ee94c302b201ecea55b26a2ea279cec00843afd
Dheeraj Singh <94373786+Dheerajsingh002@users.noreply.github.com>
2023-10-15 04:17:33 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import './App.css';
 import Layout from './components/Layout';
-
-import Dashboard from './components/Dashboard';
-
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Layout/>}>
-        <Route index element={<Dashboard />}/>
         </Route>
       </Routes>
     </Router>
